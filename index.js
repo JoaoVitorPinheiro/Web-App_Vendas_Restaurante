4e524a516bafc87256af36b82a3710211216b0d7
kurry <kurry805@gmail.com>
2024-03-06 21:18:33 -0300

    autualizando

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
-const ola = "tudo bem "
-console.log(ola);
+
+const express = require("express");
+const app= express();
+const port=3000
+
+
+app.get("/",(req,res)=>{
+    res.send("<h1>olá</h1>")
+})
+
+
+
+
+
+
+app.listen(port,(error)=>{
+    if(error){
+        console.log("servidor não iniciado ",error);
+    }else{
+        console.log(`servidor iniciado com suceeso na porta${port}`);
+    }
+})
